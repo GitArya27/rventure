b3f81ee613b2aa3e1ffa4d7d1e022bd2d4a9e579
RaginaPhalangae <115471842+RaginaPhalangae@users.noreply.github.com>
2023-03-07 03:01:26 +0530

    Update user.kt

--- app/src/main/java/com/example/r_venture/user.kt
+++ app/src/main/java/com/example/r_venture/user.kt
@@ -1,5 +1,5 @@
 package com.example.r_venture
-
+//Rventure
 data class user(val user_id : String ?=null,
                 val user_name : String ?=null,
                 val user_email : String ?=null,
